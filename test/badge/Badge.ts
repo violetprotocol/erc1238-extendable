dc0c1d742995f8cfa17d03b22802df2644f65f72
ra-phael <10075759+ra-phael@users.noreply.github.com>
2022-05-04 18:09:53 +0100

    mintBundle test

--- test/badge/Badge.ts
+++ test/badge/Badge.ts
@@ -1,5 +1,6 @@
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 import { expect } from "chai";
+import { BigNumber } from "ethers";
 import { toBn } from "evm-bn";
 import { artifacts, ethers, waffle } from "hardhat";
 import type { Artifact } from "hardhat/types";
@@ -16,10 +17,13 @@ import {
 } from "../../src/types";
 import { getMintApprovalSignature, getMintBatchApprovalSignature } from "../../src/utils/ERC1238Approval";
 
+const chainId = chainIds.hardhat;
+
 describe("Badge - Minting", function () {
   let admin: SignerWithAddress;
   let eoaRecipient1: SignerWithAddress;
-  let contractRecipient: ERC1238ReceiverMock;
+  let contractRecipient1: ERC1238ReceiverMock;
+  let contractRecipient2: ERC1238ReceiverMock;
   let badge: Badge;
   let badgeIBalance: IBalanceGettersLogic;
   let badgeMint: MintLogic;
@@ -30,9 +34,10 @@ describe("Badge - Minting", function () {
     admin = signers[0];
     eoaRecipient1 = signers[1];
 
-    // Deploy mock contract recipient
+    // Deploy mock contract recipients
     const ERC1238ReceiverMockArtifact: Artifact = await artifacts.readArtifact("ERC1238ReceiverMock");
-    contractRecipient = <ERC1238ReceiverMock>await waffle.deployContract(eoaRecipient1, ERC1238ReceiverMockArtifact);
+    contractRecipient1 = <ERC1238ReceiverMock>await waffle.deployContract(eoaRecipient1, ERC1238ReceiverMockArtifact);
+    contractRecipient2 = <ERC1238ReceiverMock>await waffle.deployContract(eoaRecipient1, ERC1238ReceiverMockArtifact);
 
     // Deploy required extensions
     const ExtendLogicFactory = await ethers.getContractFactory("ExtendLogic");
@@ -90,7 +95,7 @@ describe("Badge - Minting", function () {
         ({ v, r, s } = await getMintApprovalSignature({
           signer: eoaRecipient1,
           erc1238ContractAddress: badge.address.toLowerCase(),
-          chainId: chainIds.hardhat,
+          chainId,
           id: tokenId,
           amount: mintAmount,
         }));
@@ -121,9 +126,9 @@ describe("Badge - Minting", function () {
 
     describe("mintToContract", () => {
       it("should credit the amount of tokens", async () => {
-        await badgeMint.mintToContract(contractRecipient.address, tokenId, mintAmount, tokenURI, data);
+        await badgeMint.mintToContract(contractRecipient1.address, tokenId, mintAmount, tokenURI, data);
 
-        const balance = await badgeIBalance.balanceOf(contractRecipient.address, tokenId);
+        const balance = await badgeIBalance.balanceOf(contractRecipient1.address, tokenId);
 
         expect(balance).to.eq(mintAmount);
       });
@@ -137,7 +142,7 @@ describe("Badge - Minting", function () {
       it("should revert if the smart contract does not accept the tokens", async () => {
         // ERC1238ReceiverMock is set to reject tokens with id 0
         await expect(
-          badgeMint.mintToContract(contractRecipient.address, ethers.constants.Zero, mintAmount, tokenURI, data),
+          badgeMint.mintToContract(contractRecipient1.address, ethers.constants.Zero, mintAmount, tokenURI, data),
         ).to.be.revertedWith("ERC1238: ERC1238Receiver rejected tokens");
       });
     });
@@ -151,7 +156,7 @@ describe("Badge - Minting", function () {
         ({ v, r, s } = await getMintBatchApprovalSignature({
           signer: eoaRecipient1,
           erc1238ContractAddress: badgeMint.address,
-          chainId: chainIds.hardhat,
+          chainId,
           ids: tokenBatchIds,
           amounts: mintBatchAmounts,
         }));
@@ -169,7 +174,7 @@ describe("Badge - Minting", function () {
         ({ v, r, s } = await getMintBatchApprovalSignature({
           signer: eoaRecipient1,
           erc1238ContractAddress: badgeMint.address,
-          chainId: chainIds.hardhat,
+          chainId,
           ids: tokenBatchIds.slice(1),
           amounts: mintBatchAmounts,
         }));
@@ -203,7 +208,7 @@ describe("Badge - Minting", function () {
         await expect(
           badgeMint
             .connect(admin)
-            .mintBatchToContract(contractRecipient.address, tokenBatchIds.slice(1), mintBatchAmounts, data),
+            .mintBatchToContract(contractRecipient1.address, tokenBatchIds.slice(1), mintBatchAmounts, data),
         ).to.be.revertedWith("ERC1238: ids and amounts length mismatch");
       });
 
@@ -216,17 +221,77 @@ describe("Badge - Minting", function () {
       it("should credit the minted tokens", async () => {
         await badgeMint
           .connect(admin)
-          .mintBatchToContract(contractRecipient.address, tokenBatchIds, mintBatchAmounts, data);
+          .mintBatchToContract(contractRecipient1.address, tokenBatchIds, mintBatchAmounts, data);
 
         tokenBatchIds.forEach(async (tokenId, index) =>
-          expect(await badgeIBalance.balanceOf(contractRecipient.address, tokenId)).to.eq(mintBatchAmounts[index]),
+          expect(await badgeIBalance.balanceOf(contractRecipient1.address, tokenId)).to.eq(mintBatchAmounts[index]),
         );
       });
 
       it("should emit a MintBatch event", async () => {
-        await expect(badgeMint.mintBatchToContract(contractRecipient.address, tokenBatchIds, mintBatchAmounts, data))
+        await expect(badgeMint.mintBatchToContract(contractRecipient1.address, tokenBatchIds, mintBatchAmounts, data))
           .to.emit(badgeMint, "MintBatch")
-          .withArgs(admin.address, contractRecipient.address, tokenBatchIds, mintBatchAmounts);
+          .withArgs(admin.address, contractRecipient1.address, tokenBatchIds, mintBatchAmounts);
+      });
+    });
+
+    describe("Mint Bundle", () => {
+      let to: string[];
+      let ids: BigNumber[][];
+      let amounts: BigNumber[][];
+
+      before(() => {
+        to = [eoaRecipient1.address, contractRecipient1.address, contractRecipient2.address];
+        ids = [tokenBatchIds, tokenBatchIds.map(id => id.add(1)), tokenBatchIds.map(id => id.add(2))];
+        amounts = [mintBatchAmounts, mintBatchAmounts.map(id => id.add(3)), mintBatchAmounts.map(id => id.add(4))];
+      });
+      console.log("contractRecipient1", contractRecipient1);
+
+      it("should mint a bundle to multiple addresses", async () => {
+        const signatureFromEOA1 = await getMintBatchApprovalSignature({
+          erc1238ContractAddress: badge.address,
+          chainId,
+          signer: eoaRecipient1,
+          ids: ids[0],
+          amounts: amounts[0],
+        });
+
+        const data = [signatureFromEOA1.fullSignature, [], []];
+
+        await badgeMint.mintBundle(to, ids, amounts, [], data);
+
+        const balancesOfRecipient1: BigNumber[] = await badgeIBalance.balanceOfBatch(to[0], ids[0]);
+        balancesOfRecipient1.forEach((balance, j) => {
+          expect(balance).to.eq(amounts[0][j]);
+        });
+
+        const balancesOfRecipient2: BigNumber[] = await badgeIBalance.balanceOfBatch(to[1], ids[1]);
+        balancesOfRecipient2.forEach((balance, i) => {
+          expect(balance).to.eq(amounts[1][i]);
+        });
+
+        const balancesOfRecipient3: BigNumber[] = await badgeIBalance.balanceOfBatch(to[2], ids[2]);
+        balancesOfRecipient3.forEach((balance, i) => {
+          expect(balance).to.eq(amounts[2][i]);
+        });
+      });
+
+      it("should emit MintBatch events", async () => {
+        const signatureFromSigner1 = await getMintBatchApprovalSignature({
+          erc1238ContractAddress: badge.address,
+          chainId,
+          signer: eoaRecipient1,
+          ids: ids[0],
+          amounts: amounts[0],
+        });
+
+        const data = [signatureFromSigner1.fullSignature, [], []];
+
+        const tx = badgeMint.connect(admin).mintBundle(to, ids, amounts, [], data);
+
+        await expect(tx).to.emit(badgeIMintBaseLogic, "MintBatch").withArgs(admin.address, to[0], ids[0], amounts[0]);
+        await expect(tx).to.emit(badgeIMintBaseLogic, "MintBatch").withArgs(admin.address, to[1], ids[1], amounts[1]);
+        await expect(tx).to.emit(badgeIMintBaseLogic, "MintBatch").withArgs(admin.address, to[2], ids[2], amounts[2]);
       });
     });
   });
