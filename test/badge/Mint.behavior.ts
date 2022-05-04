5ac8dce86ce19f328ab73e9e227242a0d236ba97
ra-phael <10075759+ra-phael@users.noreply.github.com>
2022-05-04 18:12:39 +0100

    delete unused file

--- test/badge/Mint.behavior.ts
+++
@@ -1,8 +0,0 @@
-import { expect } from "chai";
-
-export function shoulMintLikeBadge(): void {
-  it("should return zero balance", async function () {
-    console.log("this", this);
-    expect(await this.badgeAsIBalanceGetters.balanceOf(this.admin.address, 1)).to.equal(0);
-  });
-}
