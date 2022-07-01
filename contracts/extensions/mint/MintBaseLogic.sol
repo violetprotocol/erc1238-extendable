1fd64348cb8434142932bae8b9c86aba3c4ccfb5
Raphael <10075759+ra-phael@users.noreply.github.com>
2022-07-01 14:17:20 +0100

    cleaning - remove unused splitSignature function (#18)

--- contracts/extensions/mint/MintBaseLogic.sol
+++ contracts/extensions/mint/MintBaseLogic.sol
@@ -219,36 +219,4 @@ contract MintBaseLogic is ERC1238Approval, IMintBaseLogic {
             revert("ERC1238: transfer to non ERC1238Receiver implementer");
         }
     }
-
-    function splitSignature(bytes memory sig)
-        internal
-        pure
-        returns (
-            bytes32 r,
-            bytes32 s,
-            uint8 v
-        )
-    {
-        require(sig.length == 65, "invalid signature length");
-
-        assembly {
-            /*
-            First 32 bytes stores the length of the signature
-
-            add(sig, 32) = pointer of sig + 32
-            effectively, skips first 32 bytes of signature
-
-            mload(p) loads next 32 bytes starting at the memory address p into memory
-            */
-
-            // first 32 bytes, after the length prefix
-            r := mload(add(sig, 32))
-            // second 32 bytes
-            s := mload(add(sig, 64))
-            // final byte (first byte of the next 32 bytes)
-            v := byte(0, mload(add(sig, 96)))
-        }
-
-        // implicitly return (r, s, v)
-    }
 }
